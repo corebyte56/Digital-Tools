41ec75f7039c2d22ad1e30207d76485351fb976b
shahariar ahammad fahim <fahim789m@gmail.com>
2026-04-01 00:03:19 +0600

    style: improve banner spacing and padding for better layout and readability

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -4,7 +4,7 @@ import Right from "./Right";
 
 const Banner = () => {
   return (
-    <div className="container mx-auto flex flex-col md:flex-row items-center justify-between gap-10 px-10 py-23">
+    <div className="container mx-auto flex flex-col md:flex-row items-center justify-between gap-10 px-10 md:py-23 py-10">
       <Left />
       <Right />
     </div>
