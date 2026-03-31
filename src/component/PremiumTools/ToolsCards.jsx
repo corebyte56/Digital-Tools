b025c65b6ecf5dc7563ca6ba9873795efbc698fc
shahariar ahammad fahim <fahim789m@gmail.com>
2026-04-01 05:28:18 +0600

    feat: create ToolsCards component with JSON fetch and useState setup

--- src/component/PremiumTools/ToolsCards.jsx
+++ src/component/PremiumTools/ToolsCards.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ToolsCards = () => {
+
+  const [showdata, setShowdata] = useState([])
+
+  const handleShowData = async () => {
+    const res = await fetch("./Tools.json")
+    const data = await res.json()
+    setShowdata(data)
+    console.log(data.data[5].description); 
+  }
+
+  useEffect(() => {
+    handleShowData();
+  }, []);
   return (
     <div>
       
