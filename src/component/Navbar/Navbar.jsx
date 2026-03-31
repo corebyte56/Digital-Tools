8eebcc0b9ac9f3662c2f8f34403709b5fc2488e4
shahariar ahammad fahim <fahim789m@gmail.com>
2026-03-31 22:38:15 +0600

    feat: improve navbar design and enhance user interface

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
   const [menuShow, setMenuShow] = useState(false);
 
   return (
-    <div>
-      <div className="container mx-auto bg-transparent sticky top-0 z-10 flex justify-between px-3 items-center gap-10">
+    <div className="border-b border-gray-300 pb-4">
+      <div className="container mx-auto bg-transparent sticky top-0 z-10 backdrop-blur-2xl  flex justify-between px-3  items-center gap-10">
         {/* name */}
         <div>
           <h1 className="font-extrabold text-3xl bg-linear-to-l from-[#3B82F6] to-[#8B5CF6] bg-clip-text text-transparent">
@@ -15,7 +15,7 @@ const Navbar = () => {
         </div>
 
         {/* menu */}
-        <div className="hidden text-[16px] font-bold md:flex items-center gap-9 list-none text-gray-500 ">
+        <div className="hidden text-[16px] font-bold lg:flex items-center gap-9 list-none text-gray-500 ">
           <li className="hover:text-[#3B82F6]">
             <a href="/">Products</a>
           </li>
@@ -34,7 +34,7 @@ const Navbar = () => {
         </div>
 
         {/*  */}
-        <div className="hidden text-sm md:flex items-center gap-9 list-none text-gray-500 text-[16px] font-bold">
+        <div className="hidden text-sm lg:flex items-center gap-9 list-none text-gray-500 text-[16px] font-bold">
           <a className="hover:text-[#3B82F6]" href="/">
             <ShoppingCart />
           </a>
@@ -52,17 +52,17 @@ const Navbar = () => {
 
         {/* mobile menu */}
         <div
-          className="md:hidden cursor-pointer p-3 hover:bg-gray-900 rounded-2xl duration-200"
+          className="lg:hidden cursor-pointer p-3 hover:bg-gray-300 rounded-2xl duration-200"
           onClick={() => setMenuShow(!menuShow)}
         >
           {menuShow ? <X color="gray" /> : <Menu size={24} color="gray" />}
 
           {menuShow && (
-            <div className="absolute text-sm right-10 font-semibold bg-gray-900 p-7  flex flex-col justify-between items-start gap-3 list-none text-gray-500 rounded-2xl shadow-2xl  z-20">
+            <div className="absolute text-sm top-15 right-10 font-semibold bg-gray-100 p-7  flex flex-col justify-between items-start gap-3 list-none text-gray-500 rounded-2xl shadow-2xl  z-20">
 
               {/* menu */}
 
-              <div className="md:hidden text-[16px] font-bold flex flex-col items-start gap-3 list-none text-gray-500 ">
+              <div className="lg:hidden text-[16px] font-bold flex flex-col items-start gap-3 list-none text-gray-500 ">
                 <li className="hover:text-[#3B82F6]">
                   <a href="/">Products</a>
                 </li>
