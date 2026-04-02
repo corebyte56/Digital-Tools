8dbfe0f1b14288e3100101ba9572f39a5a21cc4d
shahariar ahammad fahim <fahim789m@gmail.com>
2026-04-02 17:47:23 +0600

    feat: complete premium tools cards with dynamic data, responsive grid, and styled UI

---
+++ src/component/Cards/Cards.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const Cards = ({ showdata }) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {showdata.map((tool) => (
+        <div
+          key={tool.id}
+          className="relative space-y-6 flex flex-col text-start items-start border border-gray-200 py-7 px-8 rounded-2xl"
+        >
+          <img src={tool.image} className="w-16 h-16 border border-gray-300 rounded-full p-2    " />
+
+          <h3 className="text-2xl font-bold">{tool.title}</h3>
+
+          <p className="text-[#627382]">{tool.description}</p>
+
+          <div className="flex items-start">
+            <h3 className="text-2xl font-bold">{tool.price}</h3>
+            <p className="text-[#627382]">{tool.billing}</p>
+          </div>
+
+          <p
+            className={`absolute top-3 right-7 
+            text-[10px] md:text-[14px] font-semibold  px-3 py-2 rounded-full
+            ${
+              tool.badge === "Best Seller"
+                ? "bg-orange-100 text-[#BB4D00]"
+                : tool.badge === "Popular"
+                  ? "bg-purple-100 text-[#7B2CBF]"
+                  : tool.badge === "New"
+                    ? "bg-green-100  text-[#0A883E]"
+                    : "bg-white"
+            } `}
+          >
+            {tool.badge}
+          </p>
+
+          <ul>
+            {tool.features.map((feature, index) => (
+              <li
+                key={index}
+                className="text-[#627382] flex items-center gap-2"
+              >
+                {/* Svg */}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#1dba08" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check-icon lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
+
+                {/* features */}
+                {feature}
+
+              </li>
+            ))}
+          </ul>
+
+
+          {/* button */}
+
+          <button
+            className="text-white w-full text-[10px] md:text-[16px] font-extrabold md:font-semibold bg-linear-to-l from-[#9514FA] to-[#4F39F6] border border-[#3B82F6] py-3 px-4 rounded-3xl cursor-pointer drop-shadow-xl
+          hover:bg-linear-to-l hover:from-[#9514FA] hover:to-[#4F39F6] hover:bg-clip-text hover:text-transparent hover:scale-105 hover:-translate-y-1 duration-200 active:scale-95 active:translate-y-0
+          "
+          >
+            Buy Now
+          </button>
+        </div>
+
+      ))}
+    </div>
+  )
+}
+
+export default Cards
--- src/component/PremiumTools/ToolsCards.jsx
+++ src/component/PremiumTools/ToolsCards.jsx
@@ -1,70 +1,26 @@
-import { CornerUpLeftIcon, MoveRight } from 'lucide-react'
-import React, { useEffect, useState } from 'react'
+import { CornerUpLeftIcon, MoveRight } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import Cards from "../Cards/Cards";
 
 const ToolsCards = () => {
-
-  const [showdata, setShowdata] = useState([])
-  const [showBadges, setShowBadges] = useState("")
+  const [showdata, setShowdata] = useState([]);
+  const [showBadges, setShowBadges] = useState("");
 
   const handleShowData = async () => {
-    const res = await fetch("./Tools.json")
-    const data = await res.json()
-    setShowdata(data)
-  }
+    const res = await fetch("./Tools.json");
+    const data = await res.json();
+    setShowdata(data);
+  };
 
   useEffect(() => {
     handleShowData();
   }, []);
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+    
+      <Cards handleShowData={handleShowData} showdata={showdata} />
+    
+  );
+};
 
-      {showdata.map((tool) => (
-
-        <div key={tool.id} className='relative space-y-6 flex flex-col text-start items-start border border-gray-200 p-6 rounded-2xl'>
-
-          <img src={tool.image}  className='w-16 h-16' />
-
-          <h3 className='text-2xl font-bold'>{tool.title}</h3>
-
-          <p className='text-[#627382]'>{tool.description}</p>
-
-          <div className='flex items-start'>
-            <h3 className='text-2xl font-bold'>{tool.price}</h3>
-            <p className='text-[#627382]'>{tool.billing}</p>
-          </div>
-
-          <p 
-          // onScroll={()=> setShowBadges()}
-          className={`absolute top-3 right-7 
-            
-            ${showBadges === "Best Seller" ? "p-5 bg-red-600" : "py-2 px-3 rounded-full font-medium bg-green-200"}
-            `} >
-              
-            {tool.badge}
-            
-            </p>
-          <ul>
-            {tool.features.map((feature, index) => (
-              <li key={index} className='text-[#627382] flex items-center gap-2'>
-                <MoveRight color='green'/> {feature}
-              </li>
-            ))}
-          </ul>
-          {/* button */}
-
-          <button
-          className="text-white w-full text-[10px] md:text-[16px] font-extrabold md:font-semibold bg-linear-to-l from-[#9514FA] to-[#4F39F6] border border-[#3B82F6] py-3 px-4 rounded-3xl cursor-pointer drop-shadow-xl
-          hover:bg-linear-to-l hover:from-[#9514FA] hover:to-[#4F39F6] hover:bg-clip-text hover:text-transparent hover:scale-105 hover:-translate-y-1 duration-200 active:scale-95 active:translate-y-0
-          "
-        >
-          Buy Now
-        </button>
-        </div>
-      ))}
-
-    </div>
-  )
-}
-
-export default ToolsCards
+export default ToolsCards;
