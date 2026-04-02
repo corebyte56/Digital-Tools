ac34f9840f754d1a2a8a412e423ff9f5cb4885d1
shahariar ahammad fahim <fahim789m@gmail.com>
2026-04-03 01:54:46 +0600

    fix pricing cards

--- src/component/Pricing/PricingCards.jsx
+++ src/component/Pricing/PricingCards.jsx
@@ -6,7 +6,7 @@ const PricingCards = () => {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         
       {/* Pricing Card 1 */}
-      <div className="space-y-6 bg-[#F9FAFC] flex flex-col justify-between text-start items-start border border-gray-200 py-7 px-8 rounded-2xl shadow-2xl hover:shadow-xl hover:scale-105 hover:-translate-y-1 duration-200 active:scale-95 active:translate-y-0 mb-16">
+      <div className="space-y-6 h-full bg-[#F9FAFC] flex flex-col justify-between text-start items-start border border-gray-200 py-7 px-8 rounded-2xl shadow-2xl hover:shadow-xl hover:scale-105 hover:-translate-y-1 duration-200 active:scale-95 active:translate-y-0 mb-16">
         <h3 className="text-2xl font-bold">Starter</h3>
 
         <p className="text-[#627382]">Perfect for getting started</p>
@@ -48,7 +48,7 @@ const PricingCards = () => {
 
       {/* Pricing Card 2 */}
 
-      <div className="relative -top-9 space-y-6 bg-linear-to-l from-[#9514FA] to-[#4F39F6] border border-[#3B82F6] flex flex-col justify-between text-start items-start text-white py-7 px-8 rounded-2xl shadow-2xl hover:shadow-xl hover:scale-105 hover:-translate-y-1 duration-200 active:scale-95 active:translate-y-0">
+      <div className="relative h-full -top-9 space-y-6 bg-linear-to-l from-[#9514FA] to-[#4F39F6] border border-[#3B82F6] flex flex-col justify-between text-start items-start text-white py-7 px-8 rounded-2xl shadow-2xl hover:shadow-xl hover:scale-105 hover:-translate-y-1 duration-200 active:scale-95 active:translate-y-0">
 
         <p className="absolute -top-4 left-1/2 -translate-x-1/2 bg-[#FEF3C6] text-[#BB4D00] py-3 px-4 rounded-full text-sm font-medium border ">Most Popular</p>
 
@@ -92,7 +92,7 @@ const PricingCards = () => {
         {/* button */}
 
         <button
-          className="w-full text-[14px] md:text-[16px] font-extrabold py-3 px-4 rounded-3xl cursor-pointer shadow-lg hover:scale-105 hover:-translate-y-1 duration-200 active:scale-95 active:translate-y-0 bg-white 
+          className="w-full h-full text-[14px] md:text-[16px] font-extrabold py-3 px-4 rounded-3xl cursor-pointer shadow-lg hover:scale-105 hover:-translate-y-1 duration-200 active:scale-95 active:translate-y-0 bg-white 
           "
         >
           <span className="bg-linear-to-l from-[#9514FA] to-[#4F39F6] bg-clip-text text-transparent">
